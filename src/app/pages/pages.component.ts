66b6045b942c67a762327d48196cac8c295fb184
snikit <snikitz@yahoo.com>
2020-08-14 22:33:45 +0530

    sidebar with icons and avatar added

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
+import {
+  faGhost,
+  faAtlas,
+  faBolt,
+  faBookmark,
+  faBug,
+  faCommentDots,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
   styleUrls: ['./pages.component.scss'],
-  //styleUrls: ['pages.component.scss'],
+  // styleUrls: ['pages.component.scss'],
   // template: `<app-sidebar></app-sidebar>  <router-outlet></router-outlet>`,
   // template: `<app-sidebar></app-sidebar> `,
 })
 export class PagesComponent {
   menu = ['menu1', 'menu2'];
+  faGhost = faGhost;
+  faAtlas = faAtlas;
+  faBolt = faBolt;
+  faBug = faBug;
+  faBookmark = faBookmark;
+  faCommentDots = faCommentDots;
 }
+
+// $("#menu-toggle").click(function(e) {
+//   e.preventDefault();
+//   $("#wrapper").toggleClass("toggled");
+// });
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,9 +4,16 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { DemoModule } from './demo/demo.module';
 import { ThemeModule } from '../@theme/theme.module';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  imports: [PagesRoutingModule, DemoModule, ThemeModule, CommonModule],
+  imports: [
+    PagesRoutingModule,
+    DemoModule,
+    ThemeModule,
+    CommonModule,
+    FontAwesomeModule,
+  ],
   declarations: [PagesComponent],
 })
 export class PagesModule {}
